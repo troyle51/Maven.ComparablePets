c7dc93dfd09399835e265357521b85b94be53cb5
Troy <troy@ZipcodersMBP28.fios-router.home>
2022-07-30 10:41:07 -0400

    added talking pets files

--- src/main/java/io/zipcoder/Application.java
+++ src/main/java/io/zipcoder/Application.java
@@ -1,5 +1,23 @@
 package io.zipcoder;
 
 
+import java.util.List;
+import java.util.Scanner;
+
 public class Application {
+    public static Scanner input = new Scanner(System.in);
+    public static Integer numOfPets;
+    public List<Pet> petList;
+    Pet pet = new Pet();
+    public static void main(String[] args) {
+        start();
+    }
+
+    public static void start() {
+        Pet pet = new Pet();
+        pet.getNumOfPetsPrompt();
+        pet.getPetTypePrompt();
+        //pet.askPetName();
+        pet.outputPet();
+    }
 }
